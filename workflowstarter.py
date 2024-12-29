4a4b43c4c85ce2a6f54f705fd50a5d8ae08e977d
Benedikt Proeckl <benedikt.proeckl@pinksuite.io>
2024-12-29 11:11:43 +0100

    added nocleanup arg

--- workflowstarter.py
+++ workflowstarter.py
@@ -1,11 +1,24 @@
 import subprocess
-import sys
+import argparse
+import os
 
-def run_nextflow_workflow(workflow_path):
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "--cleanup":
-            command = ["nextflow", "run", workflow_path, "--cleanup"]
-    else:
+def parse():
+    parser = argparse.ArgumentParser(
+                    prog='ProgramName',
+                    description='What the program does',
+                    epilog='Text at the bottom of help')
+    parser.add_argument('-c', '--cleanup',
+                        action='store_true',
+                        help="cleans workdir")
+    parser.add_argument('-nc', '--nocleanup',
+                        action='store_true',
+                        help="all the temporary data of the workflow is saved")
+    return parser.parse_args()
+
+def run_nextflow_workflow(workflow_path, cleanup, nocleanup):
+    if cleanup:
+        command = ["nextflow", "run", workflow_path, "--cleanup"]
+    if nocleanup:
         command = ["nextflow", "run", workflow_path]
     try:
         result = subprocess.run(command, check=True, capture_output=True, text=True)
@@ -13,7 +26,14 @@ def run_nextflow_workflow(workflow_path):
     except subprocess.CalledProcessError as e:
         return f"Error: {e.stderr}"
 
-
+args = parse()
 workflow_path = "/mnt/c/Users/proec/Desktop/Spachelor/Bachelor_Arbeit/workflow.nf"
-output = run_nextflow_workflow(workflow_path)
+output = run_nextflow_workflow(workflow_path, args.cleanup, args.nocleanup)
+
+if os.path.exists(".nextflow.log"):
+    with open(".nextflow.log", "r") as logfile:
+        log = logfile.read()
+        print(log)
+    os.remove(".nextflow.log")
+
 print(output)
